ff0eeed2832a33157b048f190dbc82c62f300b11
LCAdmin3 <lcadmin3@LCAdmin3s-MacBook-Air.local>
2016-05-04 08:53:23 -0400

    first commit

---
+++ aps.js
@@ -0,0 +1,10 @@
+var name = pepperoni
+var pepperoni = 8;
+var isBurnt = false;
+
+console.log(pepperoni);
+alert(isBurnt);
+
+var amountOfFRUIT = 27;
+var strawberry = "This fruit is ready"
+var goodWeather = false;
